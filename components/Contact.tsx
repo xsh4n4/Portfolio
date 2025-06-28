7874ad722250934b1ab7097b7d4512b7ed7819e4
xsh4n4 <xsh4n4@gmail.com>
2025-06-28 23:01:11 +0530

    Fixed issue5

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -208,6 +208,12 @@ const Contact = () => {
     Get in Touch
   </h3>
 </div>
+<div className="flex items-center justify-center text-center mb-12 space-x-4">
+  <Send className="text-blue-500" size={32} />
+  <h3 className="text-4xl md:text-4xl font-bold text-white tracking-wide">
+    Get in Touch
+  </h3>
+</div>
 
 
 
