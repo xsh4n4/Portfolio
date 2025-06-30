dd76151c625e919e6de885508fa82638673816f9
xsh4n4 <xsh4n4@gmail.com>
2025-06-30 18:29:54 +0530

    Dock Fix1

--- components/magicui/Dock.tsx
+++ components/magicui/Dock.tsx
@@ -41,6 +41,19 @@ export function FloatingDockDemo() {
       icon: <IconBrandX className="h-full w-full text-white dark:text-white" />,
       href: "https://twitter.com/karaan_dev",
     },
+
+    {
+      title: "LinkedIn",
+      icon: <RiLinkedinLine className="h-full w-full text-white dark:text-white" />,
+      href: "https://www.linkedin.com/in/kendrekaran",
+    },
+    {
+      title: "Twitter",
+      icon: <IconBrandX className="h-full w-full text-white dark:text-white" />,
+      href: "https://twitter.com/karaan_dev",
+    },
+
+    
     {
       title: "GitHub",
       icon: <IconBrandGithub className="h-full w-full text-white dark:text-white" />,
