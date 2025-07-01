11aad685c0a8d1c3dadac1caec8adedbca68e6ca
xsh4n4 <xsh4n4@gmail.com>
2025-07-01 21:08:41 +0530

    Spotlight changes

--- components/magicui/Spotlight.tsx
+++ components/magicui/Spotlight.tsx
@@ -21,5 +21,23 @@ export function SpotlightPreview() {
        
       </div>
     </div>
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+       
+      </div>
+    </div>
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+       
+      </div>
+    </div>
   );
 }
